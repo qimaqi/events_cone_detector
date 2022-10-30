6c00ff59e9a4b58ebdb0b8f528fa45f026048db6
qimaqi <qimaqi@ethz.ch>
2022-10-30 19:52:00 +0100

    update readme and image

--- README.md
+++ README.md
@@ -19,3 +19,51 @@ Events time surface
 <img src="./asset/Events_detection.gif" alt="drawing" width="800"/> 
 Detection result
 
+
+## Pipeline steps
+### 1.Events data preparaton
+Take a video and simulate the events using the [esim](https://github.com/uzh-rpg/rpg_esim).
+Notice that to simulate events we normally need high frame rate video so you should move slowly when you take the video.
+
+### 2. Change it to timesurface representation
+After getting the rosbag including events you can change it to txt file for easier usage
+```
+python bag2txt file.bag
+
+python SAE_dataset_generator.py txt_path timestamps_path
+```
+You will get timesurface like 
+
+<img src="./asset/time_surface" alt="drawing" width="1000"/> 
+
+More details about timesurface representation can be checked [here](https://www.neuromorphic-vision.com/public/publications/1/publication.pdf)
+
+### 3. Prepare the labels
+Note that we still use supervised learning so we need labels for these timesurface images.
+So we generate the labels using normal yolov5
+
+
+time_surface.png
+
+#### 3.1 Training the yolov5 on the normal images to get the label
+In this step you can annotate the image youself or use the public data [FSOCO](https://www.fsoco-dataset.com/).
+After training you will get the weight. We use the weight to generate the labels for events cone detection.
+
+#### 3.2 Bootstrape the result and clean the prediction.
+Next we use the weight you get from last step run the prediction on the events corresponding images.
+
+<img src="./asset/rgb_yolo_result.gif" alt="drawing" width="800"/>  
+Yolo results.
+
+You might need to clean this labels a bit because we might have some misdetection like below images shows.
+<img src="./asset/misdetection.png" alt="drawing" width="800"/>  
+Yolo results.
+
+For cleanning you can use some label tools like [Supervisely](https://supervise.ly/)
+**Note that this step is very important **. Otherwise you might have shit in shit out XD.
+
+As you can the we move the wrong label top left.
+<img src="./asset/clean_detection.png" alt="drawing" width="800"/> 
+
+### 4. Training
+Train the weight using the timesurface image and the label come from the normal YOLO.
--- data_prepare/SAE_dataset_generator.py
+++ data_prepare/SAE_dataset_generator.py
@@ -12,115 +12,95 @@ from mpl_toolkits.mplot3d import Axes3D  # noqa: F401 unused import
 plt.close('all')
 from PIL import Image
 from tqdm import tqdm
+import argparse
 
 # %% Read a file of events and write another file with a subset of them
 #filename_sub = 'slider_depth/events_chunk.txt'
 filename_sub = './../dataset/events.txt'
 
-"""
-# This is how the file events_chunk.txt was generated from the events.txt file in the IJRR 2017 dataset
-events_raw = open('slider_depth/events.txt', "r")
-events_sub = open(filename_sub, "w")
-# format: timestamp, x, y, polarity
+# arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("txt", help="ROS bag file to extract")
+parser.add_argument("--timestamps",  help="Timestamps file path")  # /dvs/events
+parser.add_argument("--tau", default=0.3, help="decay parameter")
+parser.add_argument("--save_folder", default="/result", help="Depth map topic")
 
-for k in range(50000):
-    line = events_raw.readline()
-    #print(line)
-    events_sub.write(line)
-    
-events_raw.close()
-events_sub.close()
-"""
+args = parser.parse_args()
 
+img_size = (180,240)
+save_folder = args.save_folder
 
+def extract_data(filename):
+    infile = open(filename, 'r')
+    timestamp = []
+    x = []
+    y = []
+    pol = []
+    for line in infile:
+        words = line.split()
+        if len(words)==4:
+            timestamp.append(float(words[0]))
+            x.append(int(words[1]))
+            y.append(int(words[2]))
+            pol.append(int(words[3]))
+        else:
+            break
+    infile.close()
+    return timestamp,x,y,pol
 
-# test_list = [0,1,2,3,4]
-# t_test = 2
-# print("find_cloest_time", find_cloest_time(test_list, t_test))
-# %% Sensor size
+def find_closest_time(x,t):
+    '''
+    x timestamp array
+    t the time need to find
+    '''
+    diff_x = np.array(x) - t
+    diff_x = diff_x >= 0
+    return np.argmax(diff_x) 
 
-# Get the size of the sensor using a grayscale frame (in case of a DAVIS)
-# filename_frame = 'slider_depth/images/frame_00000000.png'
-# import cv2
-# img = cv2.imread(filename_frame, cv2.IMREAD_GRAYSCALE)
-# print img.shape
-# img = np.zeros(img.shape, np.int)
+# Call the function to read data    
+timestamp, x, y, pol = extract_data(args.txt)
 
-# For this exercise, we just provide the sensor size (height, width)
-# img_size = (180,240)
-img_size = (1080,1920)
-images_num = 2990 # 2990 image get 2989 SAE image
-fps = 60
-SAE_num = images_num-1
-timestamps_sae = np.arange(start=1, stop = 2990)/fps # sum
-tau = 1/60
-save_name_list = sorted(os.listdir('/home/qimaqi/AMZ/Events_yolov5/runs/detect/exp/labels'))
-events_npz_path = '/home/qimaqi/AMZ/Events/dataset/amz_events_result/seq1'
-events_npz_list = sorted(os.listdir(events_npz_path))
+timestamps_sae = np.genfromtxt(args.timestamps)
 
-last_index = 0
-save_folder = '/home/qimaqi/AMZ/Events/dataset/sae_detect'
-save_folder_map = '/home/qimaqi/AMZ/Events/dataset/sae_draw'
-assert len(save_name_list)-1 == len(timestamps_sae),"size mismatch"
-
-
-# img_pos = np.zeros(img_size, np.int)
-# img_neg = np.zeros(img_size, np.int)
-# for i in range(num_events):
-#     if (pol[i] > 0):
-#         img_pos[y[i],x[i]] += 1 # count events
-#     else:
-#         img_neg[y[i],x[i]] += 1
-
-# for detectioin 3000 images
-# we need 30
+last_index = 0 # default set 0 at start
 for index,timestamp_sae in tqdm(enumerate(timestamps_sae),total=len(timestamps_sae)):
     
-    if index % 100 == 0:
-        save_name = save_name_list[index+1].split('.')[0]
-        # print("save name",save_name,"timestamp_sae",timestamp_sae)
+    save_name = str(index).zfill(6)
+    
+    current_index = find_closest_time(timestamp, timestamp_sae)
 
-        events_npz_file_i = os.path.join(events_npz_path, events_npz_list[index])
-        # print("events_npz_path, events_npz_list[index]",events_npz_path, events_npz_list[index])
-        events_dict_i = np.load(events_npz_file_i)#.files()
+    timestamp_current = timestamp[last_index:current_index]
+    x_current = x[last_index:current_index]
+    y_current = y[last_index:current_index]
 
-        timestamp = events_dict_i['t']/1e9
-        x = events_dict_i['x']
-        y = events_dict_i['y']
+    img_i = np.zeros(img_size, np.float32)
 
-        img_i = np.zeros(img_size, np.float32)
-        # print("timestamp events",timestamp[-1])
-        # nearest_index = find_cloest_time(timestamp, timestamp_sae)
-        # use events between
-        # load events npz
+    for i in range(len(timestamp_current)):
+        img_i[y_current[i],x_current[i]] = np.exp(-(timestamp_sae-timestamp_current[i]) / tau)
 
+    fig = plt.figure()
+    fig.suptitle('Time surface (exp decay). Both polarities')
+    plt.imshow(img_i)
+    plt.xlabel("x [pixels]")
+    plt.ylabel("y [pixels]")
+    # plt.colorbar()
+    plt.savefig(os.path.join(save_folder_map,save_name+'.png'))
+    # plt.show()
 
-        for i in range(len(timestamp)):
-            img_i[y[i],x[i]] = np.exp(-(timestamp_sae-timestamp[i]) / tau)
+    # # change to rgb image   
+    img_i = np.expand_dims(img_i,axis=2)
+    img_i = img_i/(img_i.max()+1e-6)
+    img_rgb = np.repeat(img_i,3,axis=2)
 
-        fig = plt.figure()
-        fig.suptitle('Time surface (exp decay). Both polarities')
-        plt.imshow(img_i)
-        plt.xlabel("x [pixels]")
-        plt.ylabel("y [pixels]")
-        # plt.colorbar()
-        plt.savefig(os.path.join(save_folder_map,save_name+'.png'))
-        # plt.show()
+    # fig = plt.figure()
+    # fig.suptitle('Time surface rgb')
+    # plt.imshow(img_rgb)
+    # plt.xlabel("x [pixels]")
+    # plt.ylabel("y [pixels]")
+    # plt.show()
 
-        # # change to rgb image   
-        img_i = np.expand_dims(img_i,axis=2)
-        img_i = img_i/(img_i.max()+1e-6)
-        img_rgb = np.repeat(img_i,3,axis=2)
+    # save rgb
+    img_rgb_save = Image.fromarray(np.uint8(img_rgb*255))
+    img_rgb_save.save(os.path.join(save_folder,save_name+'.png'))
 
-        # fig = plt.figure()
-        # fig.suptitle('Time surface rgb')
-        # plt.imshow(img_rgb)
-        # plt.xlabel("x [pixels]")
-        # plt.ylabel("y [pixels]")
-        # plt.show()
-
-        # save rgb
-        img_rgb_save = Image.fromarray(np.uint8(img_rgb*255))
-        img_rgb_save.save(os.path.join(save_folder,save_name+'.png'))
-
-
+    last_time = current_index
